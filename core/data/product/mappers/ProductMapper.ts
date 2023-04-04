b9a600e90b3bbf5ceb53053ee2fb99a5b3a59db2
alfanfauzy <zhafhy@gmail.com>
2023-04-04 14:45:37 +0700

    fix(delete): remove file

--- core/data/product/mappers/ProductMapper.ts
+++
@@ -1,6 +0,0 @@
-import { GetProductMenuResponse } from '../types'
-import { Products, ProductsMenu } from 'core/domain/product/models'
-
-export const mapToProductMenuListModel = (datas: GetProductMenuResponse): ProductsMenu => {
-  datas.objs.map((data) => ({}))
-}
