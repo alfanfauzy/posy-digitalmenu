47371c3282d4d0f63d4715bfe162e5cbd45108d0
afauzy <afauzy.pvg.co.id>
2023-07-24 13:24:36 +0700

    fix(utils): fix utils capitalize

--- src/utils/UtilsCapitalizeLetter.ts
+++ src/utils/UtilsCapitalizeLetter.ts
@@ -1,3 +1,15 @@
 export const capitalizeFirstLetter = (str: string) => {
-	return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+	const words = str.split(' ');
+
+	// Capitalize the first letter of each word and convert the rest to lowercase
+	const capitalizedWords = words.map(word => {
+		const firstLetter = word.charAt(0).toUpperCase();
+		const restOfWord = word.slice(1).toLowerCase();
+		return firstLetter + restOfWord;
+	});
+
+	// Join the words back together to form the final result
+	const convertedString = capitalizedWords.join(' ');
+
+	return convertedString;
 };
