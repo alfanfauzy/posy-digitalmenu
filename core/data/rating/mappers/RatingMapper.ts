95f5ac417ada806475fc0f86c57ae215517c5637
afauzy <afauzy.pvg.co.id>
2023-07-24 13:21:06 +0700

    fix(rating): fix mapping history rating

--- core/data/rating/mappers/RatingMapper.ts
+++ core/data/rating/mappers/RatingMapper.ts
@@ -7,12 +7,13 @@ export const mapToRatingsByProductModel = (
 	datas: Array<GetRatingByProductResponse>,
 ): RatingsByProduct =>
 	datas.map(data => ({
-		customer_name: data.customer?.name,
-		date: data.metadata.created_at?.seconds as number,
-		food_rating_uuid: data.food_rating_uuid,
-		rating: data.rating,
-		product_name: data.product_name,
-		review: data.review
+		customer_name: data.customer_info.name,
+		date: data.customer_info.first_order_at,
+		food_rating_uuid: data.food_rating_product.uuid,
+		rating: data.food_rating_product.rating,
+		product_name: data.food_rating_product.product_name,
+		review: data.food_rating_product.review
 			.filter(val => val !== 'OTHERS')
 			.map(text => capitalizeFirstLetter(text.split('_').join(' '))),
+		review_note: data.food_rating_product.review_note,
 	}));
--- core/data/rating/types/GetRatingsByProductType.ts
+++ core/data/rating/types/GetRatingsByProductType.ts
@@ -1,26 +1,14 @@
-import {MetadataObject} from '@/domain/vo/BaseMetadata';
-
-type Variant = {
-	variant_name: string;
-	variant_price: number;
-};
-
-type AddonInformation = {
-	addon_name: string;
-	addon_price: number;
-	addon_variants: Array<Variant>;
-};
-
 export type GetRatingByProductResponse = {
-	uuid: string;
-	food_rating_uuid: string;
-	product_uuid: string;
-	product_name: string;
-	addon_information: Array<AddonInformation>;
-	customer: {
+	customer_info: {
 		name: string;
+		first_order_at: string;
 	};
-	rating: number;
-	review: Array<string>;
-	metadata: MetadataObject;
+	food_rating_product: {
+		uuid: string;
+		product_name: string;
+		addon_information: any;
+		rating: number;
+		review: Array<string>;
+		review_note: string;
+	};
 };
